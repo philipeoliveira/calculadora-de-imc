c494067e60b1d96d2fb7c5bb42daf80354fb36a1
Philipe Oliveira <philipeoliveira.ti@gmail.com>
2024-05-02 18:38:39 -0300

    Change the inputs type to text

--- src/Input.tsx
+++ src/Input.tsx
@@ -5,7 +5,7 @@ export function Input(props: ComponentProps<'input'>) {
       <>
          <input
             {...props}
-            type='number'
+            type='text'
             className='w-full p-2 px-3 bg-transparent rounded-lg outline-none border border-slate-400 hover:border-white focus:border-white text-white'
          />
       </>
